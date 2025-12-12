9e9cb01c113d90ee65d34ec40bc9454e270c7546
Sandeep Kumar Penta <tsbsandeepkumar@gmail.com>
2025-12-12 22:16:00 +0530

    Terraform resource nd provider

---
+++ main.tf
@@ -0,0 +1,18 @@
+resource "aws_instance" "frontend" {
+    ami = "ami-09c813fb71547fc4f"
+    instance_type = "t3.micro"
+    tags = {
+    Name = "HelloWorld"
+    }
+}
+
+resource "aws_route53_record" "frontend" {
+  zone_id = "Z09354891N46GVLJSDZH0"
+  name    = "frontend-dev"
+  type    = "A"
+  ttl     = 30
+
+  records = [aws_instance.web.private_ip]
+
+  depends_on = [aws_instance.web]
+}
