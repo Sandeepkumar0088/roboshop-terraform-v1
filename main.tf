d5c7bff050a56d7965f9f00968caaa64c7d4a74b
Sandeep Kumar Penta <tsbsandeepkumar@gmail.com>
2025-12-12 22:23:39 +0530

    Terraform resource

--- main.tf
+++ main.tf
@@ -2,17 +2,33 @@ resource "aws_instance" "frontend" {
     ami = "ami-09c813fb71547fc4f"
     instance_type = "t3.micro"
     tags = {
-    Name = "HelloWorld"
+    Name = "frontend"
     }
 }
 
-resource "aws_route53_record" "frontend" {
+resource "aws_route53_record" "frontend-record" {
   zone_id = "Z09354891N46GVLJSDZH0"
   name    = "frontend-dev"
   type    = "A"
   ttl     = 30
 
-  records = [aws_instance.frontend.private_ip]
-
-  depends_on = [aws_instance.frontend]
+  records = [aws_instance.frontend-record.private_ip]
 }
+
+resource "aws_instance" "mongodb" {
+    ami = "ami-09c813fb71547fc4f"
+    instance_type = "t3.micro"
+    tags = {
+    Name = "mongodb"
+    }
+}
+
+resource "aws_route53_record" "mongodb-record" {
+  zone_id = "Z09354891N46GVLJSDZH0"
+  name    = "mongodb-dev"
+  type    = "A"
+  ttl     = 30
+
+  records = [aws_instance.mongodb-record.private_ip]
+}
+
