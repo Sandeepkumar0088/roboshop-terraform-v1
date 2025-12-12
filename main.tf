851a58456653d87ae84de537b3b116391cefef3a
Sandeep Kumar Penta <tsbsandeepkumar@gmail.com>
2025-12-12 22:18:12 +0530

    Terraform resource

--- main.tf
+++ main.tf
@@ -12,7 +12,7 @@ resource "aws_route53_record" "frontend" {
   type    = "A"
   ttl     = 30
 
-  records = [aws_instance.web.private_ip]
+  records = [aws_instance.frontend.private_ip]
 
   depends_on = [aws_instance.web]
 }
