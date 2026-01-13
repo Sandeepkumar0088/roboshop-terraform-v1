7459c59ec3e9e9c09cc51f5b7b4ad1bde4d1a904
Sandeep Kumar Penta <tsbsandeepkumar@gmail.com>
2026-01-13 10:28:23 +0530

    terraform

--- main.tf
+++ main.tf
@@ -1,54 +1,70 @@
-resource "aws_instance" "frontend" {
-    ami = "ami-09c813fb71547fc4f"
-    instance_type = "t3.micro"
-    vpc_security_group_ids= [ "sg-039df2e97e51ebc17" ]
-    tags = {
-    Name = "frontend"
-    }
+# resource "aws_instance" "frontend" {
+#   ami = "ami-09c813fb71547fc4f"
+#   instance_type = "t3.micro"
+#   vpc_security_group_ids= [ "sg-039df2e97e51ebc17" ]
+#   tags = {
+#   Name = "frontend"
+#   }
+# }
+#
+# resource "aws_route53_record" "frontend" {
+#   zone_id = "Z09354891N46GVLJSDZH0"
+#   name    = "frontend-dev"
+#   type    = "A"
+#   ttl     = 30
+#
+#   records = [aws_instance.frontend.private_ip]
+# }
+#
+# resource "aws_instance" "mongodb" {
+#     ami = "ami-09c813fb71547fc4f"
+#     instance_type = "t3.micro"
+#     vpc_security_group_ids= [ "sg-039df2e97e51ebc17" ]
+#     tags = {
+#     Name = "mongodb"
+#     }
+# }
+#
+# resource "aws_route53_record" "mongodb" {
+#   zone_id = "Z09354891N46GVLJSDZH0"
+#   name    = "mongodb-dev"
+#   type    = "A"
+#   ttl     = 30
+#
+#   records = [aws_instance.mongodb.private_ip]
+# }
+#
+# resource "aws_instance" "catalogue" {
+#     ami = "ami-09c813fb71547fc4f"
+#     instance_type = "t3.micro"
+#     vpc_security_group_ids= [ "sg-039df2e97e51ebc17" ]
+#     tags = {
+#     Name = "catalogue"
+#     }
+# }
+#
+# resource "aws_route53_record" "catalogue" {
+#   zone_id = "Z09354891N46GVLJSDZH0"
+#   name    = "catalogue-dev"
+#   type    = "A"
+#   ttl     = 30
+#   records = [aws_instance.catalogue.private_ip]
+# }
+
+resource "aws_instance" "shipping"{
+  ami = "ami-09c813fb71547fc4f"
+  instance_type = "t3.small"
+  vpc_security_group_ids = [ "sg-039df2e97e51ebc17" ]
+
+  tags = {
+    Name = "shipping"
+  }
 }
 
-resource "aws_route53_record" "frontend" {
+resource "aws_route53_record" "shipping" {
   zone_id = "Z09354891N46GVLJSDZH0"
-  name    = "frontend-dev"
-  type    = "A"
-  ttl     = 30
-
-  records = [aws_instance.frontend.private_ip]
+  name = "shipping-dev"
+  ttl = 5
+  type = "A"
+  records = [ aws_instance.shipping ]
 }
-
-resource "aws_instance" "mongodb" {
-    ami = "ami-09c813fb71547fc4f"
-    instance_type = "t3.micro"
-    vpc_security_group_ids= [ "sg-039df2e97e51ebc17" ]
-    tags = {
-    Name = "mongodb"
-    }
-}
-
-resource "aws_route53_record" "mongodb" {
-  zone_id = "Z09354891N46GVLJSDZH0"
-  name    = "mongodb-dev"
-  type    = "A"
-  ttl     = 30
-
-  records = [aws_instance.mongodb.private_ip]
-}
-
-resource "aws_instance" "catalogue" {
-    ami = "ami-09c813fb71547fc4f"
-    instance_type = "t3.micro"
-    vpc_security_group_ids= [ "sg-039df2e97e51ebc17" ]
-    tags = {
-    Name = "catalogue"
-    }
-}
-
-resource "aws_route53_record" "catalogue" {
-  zone_id = "Z09354891N46GVLJSDZH0"
-  name    = "catalogue-dev"
-  type    = "A"
-  ttl     = 30
-
-  records = [aws_instance.catalogue.private_ip]
-}
-
