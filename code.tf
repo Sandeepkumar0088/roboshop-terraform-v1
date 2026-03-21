b5b51e9cea22f2eaf7316a4f3550b865f8a441c9
Sandeep Kumar Penta <tsbsandeepkumar@gmail.com>
2026-03-21 22:38:11 +0530

    terraform for_each loop

--- code.tf
+++ code.tf
@@ -1,22 +1,28 @@
 resource "aws_instance" "instance" {
-  count                   = 2
+  for_each                = var.components
 
   ami                     = "ami-0220d79f3f480ecf5"
   instance_type           = "t3.micro"
   vpc_security_group_ids  = ["sg-080ee07db03cf22ab"]
   tags = {
-    Name = var.components[count.index]
+    Name = each.key
   }
 }
 
 resource "aws_route53_record" "dns" {
-  count   = 2
-  zone_id = "Z09354891N46GVLJSDZH0"
-  ttl     = 5
-  type    = "A"
-  name    = "${var.components[count.index]}-dev"
-  records = [aws_instance.instance[count.index].private_ip]
+  for_each  = var.components
+
+  zone_id   = "Z09354891N46GVLJSDZH0"
+  ttl       = 5
+  type      = "A"
+  name      = "${each.key}-dev"
+  records   = [aws_instance.instance[each.key].private_ip]
 }
 
 
-variable components { default = [ "black", "white" ] }
+variable components {
+  default = {
+    black = "t3.micro"
+    white = "t3.micro"
+  }
+}
