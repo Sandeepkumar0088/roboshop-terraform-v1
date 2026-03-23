65e0a799676e0321adc9b10510f2a3a5cdb39178
Sandeep Kumar Penta <tsbsandeepkumar@gmail.com>
2026-03-23 13:01:01 +0530

    terraform provisioner

--- code.tf
+++ code.tf
@@ -1,3 +1,11 @@
+terraform {
+  required_providers {
+    null = {
+      source  = "hashicorp/null"
+      version = "3.2.4"
+    }
+  }
+}
 resource "aws_instance" "instance" {
   for_each                = var.components
 
@@ -24,5 +32,25 @@ variable components {
   default = {
     black = "t3.micro"
     white = "t3.micro"
+  }
+}
+
+
+resource "null_resource" "ansible" {
+  for_each  = var.components
+
+  depends_on = [aws_route53_record.dns,aws_instance.instance]
+
+  provisioner "remote-exec" {
+    connection {
+      type = "ssh"
+      user = "ec2"
+      password = ""
+      host     = aws_instance.instance[each.key].private_ip
+    }
+    inline = [
+      "pip3.13 install ansible",
+      "amsible-pull -i localhost, -U githuburl main.yml -e component=${each.key} -e env=dev"
+    ]
   }
 }
